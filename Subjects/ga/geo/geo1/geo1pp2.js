7cc0f47e2beb16bacd241a0c9ab9cea7640b8dee
Piyush Raj <interact.piyush@gmail.com>
2025-07-31 20:29:30 +0530

    new test added to solar system
    and subject which don't have contents are made hidden

---
+++ Subjects/ga/geo/geo1/geo1pp2.js
@@ -0,0 +1,313 @@
+const questions = [
+{
+    question:"The ______ is responsible for Earth's magnetic field.",
+    answers:[
+        {text: "inner core", correct: false},
+        {text: "mantle", correct: false},
+        {text: "outer core", correct: true},
+        {text: "crust", correct: false},
+    ]
+},
+{
+    question:"The moon is an example of a ______ object.",
+    answers:[
+        {text: "non-luminous", correct: true},
+        {text: "luminous", correct: false},
+        {text: "translucent", correct: false},
+        {text: "transparent", correct: false},
+    ]
+},
+{
+    question:"The process that moves, elevates or builds up portions of the earth's crust is called:",
+    answers:[
+        {text: "diastrophism", correct: true},
+        {text: "weathering", correct: false},
+        {text: "volcanism", correct: false},
+        {text: "denudation", correct: false},
+    ]
+},
+{
+    question:"When are neap tides produced?",
+    answers:[
+        {text: "When the sun and moon are at right angles to the earth.", correct: true},
+        {text: "when the sun lowers the gravitational pull of the moon.", correct: false},
+        {text: "When the earth and moon are in a straight line.", correct: false},
+        {text: "When the sun partially contracts pull of the earth", correct: false},
+    ]
+},
+{
+    question:"Which of the following planets is known as a gas giant?",
+    answers:[
+        {text: "Earth", correct: false},
+        {text: "Mercury", correct: false},
+        {text: "Venus", correct: false},
+        {text: "Jupiter", correct: true},
+    ]
+},
+{
+    question:"The bodies glowing in the night sky are known as ________ .",
+    answers:[
+        {text: "Meteorite", correct: false},
+        {text: "Planetarium", correct: false},
+        {text: "Asteroids", correct: false},
+        {text: "Celestial bodies", correct: true},
+    ]
+},
+{
+    question:"Lucifer is another name for the planet ______.",
+    answers:[
+        {text: "Saturn", correct: false},
+        {text: "Venus", correct: true},
+        {text: "Mars", correct: false},
+        {text: "Jupiter", correct: false},
+    ]
+},
+{
+    question:"Planetary Scientists call the thin gaseous envelope around the Moon as the _______ .",
+    answers:[
+        {text: "lunar endosphere", correct: false},
+        {text: "lunar thermosphere", correct: false},
+        {text: "lunar stratosphere", correct: false},
+        {text: "lunar exosphere", correct: true},
+    ]
+},
+{
+    question:"In the sequence of planets in the solar system, which planet comes in between Mars and Saturn?",
+    answers:[
+        {text: "Jupiter", correct: true},
+        {text: "Uranus", correct: false},
+        {text: "Mercury", correct: false},
+        {text: "Venus", correct: false},
+    ]
+},
+{
+    question:"How many degrees does the Earth rotate about its own axis in one hour?",
+    answers:[
+        {text: "15", correct: true},
+        {text: "24", correct: false},
+        {text: "20", correct: false},
+        {text: "10", correct: false},
+    ]
+},
+{
+    question:"Which of the following is the outermost solid part of the Earth?",
+    answers:[
+        {text: "Caldera", correct: false},
+        {text: "Core", correct: false},
+        {text: "Crust", correct: true},
+        {text: "Mantle", correct: false},
+    ]
+},
+{
+    question:"Which of the following is known as the study of earthquakes?",
+    answers:[
+        {text: "Histology", correct: false},
+        {text: "Semiology", correct: false},
+        {text: "Lithology", correct: false},
+        {text: "Seismology", correct: true},
+    ]
+},
+{
+    question:"Which of the following is the outer layer of the Earth that is made of plates which fit together like a jigsaw puzzle?",
+    answers:[
+        {text: "Lithosphere", correct: true},
+        {text: "Biosphere", correct: false},
+        {text: "Mesosphere", correct: false},
+        {text: "Asthenosphere", correct: false},
+    ]
+},
+{
+    question:"The solar eclipse occurs when:",
+    answers:[
+        {text: "the moon comes between the earth and the sun", correct: true},
+        {text: "the sun comes between the moon and the earth", correct: false},
+        {text: "an asteroid comes between the sun and the moon", correct: false},
+        {text: "the moon moves too far away from the sun", correct: false},
+    ]
+},
+{
+    question:"Europa is the natural satellite of:",
+    answers:[
+        {text: "Saturn", correct: false},
+        {text: "Uranus", correct: false},
+        {text: "Jupiter", correct: true},
+        {text: "Mars", correct: false},
+    ]
+},
+{
+    question:"In the life cycle of a star, the stage nebula refers to:",
+    answers:[
+        {text: "the stage when the outer layers expand, cool down and become less bright", correct: false},
+        {text: "the last stage of its life", correct: false},
+        {text: "a cloud of dust and hydrogen", correct: true},
+        {text: "the outer shell of a star", correct: false},
+    ]
+},
+{
+    question:"Which of the following planet is NOT a jovian planet?",
+    answers:[
+        {text: "Jupiter", correct: false},
+        {text: "Mars", correct: true},
+        {text: "Uranus", correct: false},
+        {text: "Saturn", correct: false},
+    ]
+},
+{
+    question:"Baily's beads can be observed during a/an:",
+    answers:[
+        {text: "occultation of Jupiter", correct: false},
+        {text: "partial solar eclipse", correct: false},
+        {text: "lunar eclipse", correct: false},
+        {text: "total solar eclipse", correct: true},
+    ]
+},
+{
+    question:"The weight of an object on the moon is equal to _______ of its weight on the earth.",
+    answers:[
+        {text: "1/10th", correct: false},
+        {text: "1/6th", correct: true},
+        {text: "1/8th", correct: false},
+        {text: "1/4th", correct: false},
+    ]
+},
+{
+    question:"Which of the following statements about Ganymede is NOT correct?",
+    answers:[
+        {text: "It is bigger than Mercury.", correct: false},
+        {text: "It is the largest natural satellite in our solar system.", correct: false},
+        {text: "It is the only moon known to have its own internally generated magnetic field.", correct: false},
+        {text: "It revolves around Saturn.", correct: true},
+    ]
+},
+{
+    question:"What is the periodicity of Halley's comet?",
+    answers:[
+        {text: "30-31 years", correct: false},
+        {text: "45-46 years", correct: false},
+        {text: "75-76 years", correct: true},
+        {text: "85-86 years", correct: false},
+    ]
+},
+{
+    question:"Which is the second largest planet in the solar system?",
+    answers:[
+        {text: "Uranus", correct: false},
+        {text: "Jupiter", correct: false},
+        {text: "Saturn", correct: true},
+        {text: "Neptune", correct: false},
+    ]
+},
+{
+    question:" Which of the following is a small solar system body containing large quantity of ice?",
+    answers:[
+        {text: "Meteors", correct: false},
+        {text: "Asteroids", correct: false},
+        {text: "Comets", correct: true},
+        {text: "Quasar", correct: false},
+    ]
+},
+{
+    question:"Which planet has the highest density in our solar system?",
+    answers:[
+        {text: "Mercury", correct: false},
+        {text: "Earth", correct: true},
+        {text: "Venus", correct: false},
+        {text: "Mars", correct: false},
+    ]
+},
+{
+    question:"Which planet is known as red planet in our solar system?",
+    answers:[
+        {text: "Mars", correct: true},
+        {text: "Venus", correct: false},
+        {text: "Saturn", correct: false},
+        {text: "Mercury", correct: false},
+    ]
+},
+
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuize(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;  
+
+    currentQuestion.answers.forEach(answer => {
+       const button = document.createElement("button");
+       button.innerHTML = answer.text;
+       button.classList.add("ans-btn");
+       answerButtons.appendChild(button); 
+       if(answer.correct){
+        button.dataset.correct = answer.correct;
+       }
+       button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+}else{
+    selectedBtn.classList.add("incorrect");
+}
+Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+    }
+    button.disabled = true;
+});
+nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+handleNextButton();
+    }else{
+        startQuize();
+    }
+});
+
+startQuize();
+
