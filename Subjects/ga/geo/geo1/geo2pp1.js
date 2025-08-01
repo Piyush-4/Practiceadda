d4b96dc387188ce06a32c73005db9deb344f2297
Piyush Raj <interact.piyush@gmail.com>
2025-08-01 15:01:24 +0530

    new test added to geography > longitudes and latitudes

---
+++ Subjects/ga/geo/geo1/geo2pp1.js
@@ -0,0 +1,311 @@
+const questions = [
+{
+    question:"Which of the following parallels of latitude represent the Tropic of Capricorn?",
+    answers:[
+        {text: "23&#189;&deg; in the Northern Hemisphere", correct: false},
+        {text: "66&#189;&deg; in the Southern Hemisphere", correct: false},
+        {text: "23&#189;&deg; in the Southern Hemisphere", correct: true},
+        {text: "66&#189;&deg; in the Northern Hemisphere", correct: false},
+    ]
+},
+{
+    question:"The Ganga-Brahmaputra basin lies between ________ latitudes",
+    answers:[
+        {text: "16&deg;N and 36&deg;N", correct: false},
+        {text: "14&deg;N and 34&deg;N", correct: false},
+        {text: "12&deg;N and 32&deg;N", correct: false},
+        {text: "10&deg;N and 30&deg;N", correct: true},
+    ]
+},
+{
+    question:"The Tropic of Cancer does NOT pass through which of the following Indian states?",
+    answers:[
+        {text: "Chhattisgarh", correct: false},
+        {text: "Assam", correct: true},
+        {text: "Tripura", correct: false},
+        {text: "Jharkhand", correct: false},
+    ]
+},
+{
+    question:"Which longitude has been selected as the Standard Meridian of India?",
+    answers:[
+        {text: "81&deg;30'E", correct: false},
+        {text: "82&deg;30'E", correct: true},
+        {text: "82&deg;32'E", correct: false},
+        {text: "82&deg;31'E", correct: false},
+    ]
+},
+{
+    question:"The 90&deg; East Meridian does NOT pass through:",
+    answers:[
+        {text: "Meghalaya", correct: false},
+        {text: "Sikkim", correct: true},
+        {text: "Bhutan", correct: false},
+        {text: "Bangladesh", correct: false},
+    ]
+},
+{
+    question:"The Indian Standard Time corresponds to the longitude _______ degrees East.",
+    answers:[
+        {text: "85", correct: true},
+        {text: "85&#189;", correct: false},
+        {text: "76&#189;", correct: false},
+        {text: "79&#189;", correct: false},
+    ]
+},
+{
+    question:"India is located to the North of the ___________ .",
+    answers:[
+        {text: "Tropic of Capricorn", correct: false},
+        {text: "Tropic of Cancer", correct: false},
+        {text: "Equator", correct: true},
+        {text: "Prime Meridian", correct: false},
+    ]
+},
+{
+    question:"1&deg; latitude is equal to approximately _______ Km.",
+    answers:[
+        {text: "111", correct: true},
+        {text: "145", correct: false},
+        {text: "133", correct: false},
+        {text: "122", correct: false},
+    ]
+},
+{
+    question:"On which day of the year is the earth farthest from the sun, at a position called aphelion?",
+    answers:[
+        {text: "19 August", correct: false},
+        {text: "23 October", correct: false},
+        {text: "12 May", correct: false},
+        {text: "4 July", correct: true},
+    ]
+},
+{
+    question:"Lines joining located at equal travel time from a common centre are called:",
+    answers:[
+        {text: "Isohalines", correct: true},
+        {text: "Isochrones", correct: false},
+        {text: "Isobar", correct: false},
+        {text: "Isobronts", correct: false},
+    ]
+},
+{
+    question:"The Tropic of Cancer does NOT pass through which of the following Indian states?",
+    answers:[
+        {text: "Madhya Pradesh", correct: false},
+        {text: "Andhra Pradesh", correct: true},
+        {text: "West Bengal", correct: false},
+        {text: "Gujarat", correct: false},
+    ]
+},
+{
+    question:"All parallel circles from the Equator to the poles are known as_______",
+    answers:[
+        {text: "Parallels of Latitude", correct: true},
+        {text: "Tropic of Cancer", correct: false},
+        {text: "Arctic Circle", correct: false},
+        {text: "Antarctic Circle", correct: false},
+    ]
+},
+{
+    question:"When does the entire earth experience equal days and nights?",
+    answers:[
+        {text: "Day of winter solstice", correct: false},
+        {text: "Day of summer solstice", correct: false},
+        {text: "At orbital plane", correct: false},
+        {text: "Day of equinox", correct: true},
+    ]
+},
+{
+    question:"Name the state capital located 53 metres above the sea level between 93&deg; East longitude and 27&deg; North latitude.",
+    answers:[
+        {text: "Dispur", correct: false},
+        {text: "Patna", correct: false},
+        {text: "Itanagar", correct: true},
+        {text: "Hyderabad", correct: false},
+    ]
+},
+{
+    question:"The Physics Phenomenon when, in Polar regions like Alaska and Northern Canada, a splendid display of colours is seen in the sky is called a/an:",
+    answers:[
+        {text: "aurora borealis", correct: true},
+        {text: "star shower", correct: false},
+        {text: "active galactic nucleus", correct: false},
+        {text: "star's intrinsic luminosity", correct: false},
+    ]
+},
+{
+    question:"Which among the following has its latitude within one degree of that of New Delhi?",
+    answers:[
+        {text: "Amarnath Cave", correct: false},
+        {text: "City of Islamabad", correct: false},
+        {text: "City of Dhaka", correct: false},
+        {text: "Mount Everest", correct: true},
+    ]
+},
+{
+    question:"'Medicine line' is another name of which among the following circles of latitude? ",
+    answers:[
+        {text: "17th Parallel North", correct: false},
+        {text: "39th Parallel North", correct: false},
+        {text: "49th Parallel North", correct: true},
+        {text: "59th Parallel North", correct: false},
+    ]
+},
+{
+    question:"The Tamil Nadu State is located between the latitudes",
+    answers:[
+        {text: "18&deg;05' to 23&deg;09' North", correct: false},
+        {text: "8&deg;05' to 13&deg;09' North", correct: true},
+        {text: "8&deg;05' to 23&deg;09' North", correct: false},
+        {text: "13&deg;09' to 23&deg;09' North", correct: false},
+    ]
+},
+{
+    question:"Which of the following zone is situated between 66&deg;30' S and poles?",
+    answers:[
+        {text: "Torrid Zone", correct: false},
+        {text: "Temperate Zone", correct: false},
+        {text: "Frigid Zone", correct: true},
+        {text: "All of these", correct: false},
+    ]
+},
+{
+    question:"Which of the following is called the Prime Meridian?",
+    answers:[
+        {text: "23&deg;30' N", correct: false},
+        {text: "23&deg;30' S", correct: false},
+        {text: "82&deg;30' E", correct: false},
+        {text: "0&deg; longitude", correct: true},
+    ]
+},
+{
+    question:"What divides the earth into the eastern and the western hemispheres?",
+    answers:[
+        {text: "Equator", correct: false},
+        {text: "Prime Meridian", correct: true},
+        {text: "82&deg;30' E", correct: false},
+        {text: "None of these.", correct: false},
+    ]
+},
+{
+    question:"The time of India is ahead of that of England by",
+    answers:[
+        {text: "2 hours", correct: false},
+        {text: "5 hour and 30 minutes", correct: true},
+        {text: "3 hours", correct: false},
+        {text: "none of these", correct: false},
+    ]
+},
+{
+    question:"Name one of the following parallels of latitudes as Tropic of Cancer.",
+    answers:[
+        {text: "0&deg;", correct: false},
+        {text: "23&deg;30' S", correct: false},
+        {text: "23&deg;30' N", correct: true},
+        {text: "66&deg;30' N", correct: false},
+    ]
+},
+{
+    question:"The total number of meridians across the globe is",
+    answers:[
+        {text: "180", correct: false},
+        {text: "360", correct: true},
+        {text: "90", correct: false},
+        {text: "181", correct: false},
+    ]
+},
+{
+    question:"Through which of the following states does the Tropic of Cancer run?",
+    answers:[
+        {text: "Mizoram", correct: true},
+        {text: "Assam", correct: false},
+        {text: "Nagaland", correct: false},
+        {text: "Manipur", correct: false},
+    ]
+},
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuize(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;  
+
+    currentQuestion.answers.forEach(answer => {
+       const button = document.createElement("button");
+       button.innerHTML = answer.text;
+       button.classList.add("ans-btn");
+       answerButtons.appendChild(button); 
+       if(answer.correct){
+        button.dataset.correct = answer.correct;
+       }
+       button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+}else{
+    selectedBtn.classList.add("incorrect");
+}
+Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+    }
+    button.disabled = true;
+});
+nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+handleNextButton();
+    }else{
+        startQuize();
+    }
+});
+
+startQuize();
+
