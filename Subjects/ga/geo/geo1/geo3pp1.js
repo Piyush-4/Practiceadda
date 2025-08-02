f970c5196e7402054771285846f1e57a71341821
Piyush Raj <interact.piyush@gmail.com>
2025-08-02 12:17:30 +0530

    new test added to continents and oceans chapter

---
+++ Subjects/ga/geo/geo1/geo3pp1.js
@@ -0,0 +1,311 @@
+const questions = [
+{
+    question:"Which water body covers an area of 1,55,58,000 km<sup>2</sup> and makes up only 4.3% of the global ocean?",
+    answers:[
+        {text: "Atlantic Ocean", correct: false},
+        {text: "Southern Ocean", correct: false},
+        {text: "Arctic Ocean", correct: true},
+        {text: "Indian Ocean", correct: false},
+    ]
+},
+{
+    question:"Which is the largest continental shelf in the world?",
+    answers:[
+        {text: "The shelf of India", correct: false},
+        {text: "The Siberian shelf in the Arctic Ocean", correct: true},
+        {text: "The Indian Ocean shelf", correct: false},
+        {text: "The shelf in the Pacific Ocean", correct: false},
+    ]
+},
+{
+    question:"Which outside layer of the Earth is less dense, thick, and andesitic to granite composition",
+    answers:[
+        {text: "Oceanic crust", correct: false},
+        {text: "Mantle", correct: false},
+        {text: "Continental crust", correct: true},
+        {text: "Outer core", correct: false},
+    ]
+},
+{
+    question:"Which is the fourth largest water body in the world recognised by the US Board on Geographic Names as a body of water extending off the coast of Antarctica?",
+    answers:[
+        {text: "Southern Ocean", correct: true},
+        {text: "Indian Ocean", correct: false},
+        {text: "Arctic Ocean", correct: false},
+        {text: "Atlantic Ocean", correct: false},
+    ]
+},
+{
+    question:"Which sea is the north-western part of the Indian Ocean that covers a total area of approximately 14,91,000 square miles and forms a part of the major sea route between Europe and India?",
+    answers:[
+        {text: "Arabian Sea", correct: true},
+        {text: "Caribbean Sea", correct: false},
+        {text: "Caspian Sea", correct: false},
+        {text: "Mediterranean Sea", correct: false},
+    ]
+},
+{
+    question:"Identify the factor that does NOT affect the salinity of the ocean.",
+    answers:[
+        {text: "The type of marine organisms found in the water bodies", correct: true},
+        {text: "The rate of evaporation and precipitation", correct: false},
+        {text: "Wind", correct: false},
+        {text: "The amount of fresh water flowing into the ocean", correct: false},
+    ]
+},
+{
+    question:"What happens to the salinity at the surface of the ocean?",
+    answers:[
+        {text: "Increases by the precipitation process", correct: false},
+        {text: "Decreases by the loss of water to ice or evaporation", correct: false},
+        {text: "Increases by the loss of water to ice or evaporation", correct: true},
+        {text: "Decreases by transpiration", correct: false},
+    ]
+},
+{
+    question:"Identify the shallowest part of the ocean.",
+    answers:[
+        {text: "Continental shelf", correct: true},
+        {text: "Continental slope", correct: false},
+        {text: "Deep sea plains", correct: false},
+        {text: "Trenches", correct: false},
+    ]
+},
+{
+    question:"Which water body is the world's third largest ocean spread over an area of 70,560,000 km<sup>2</sup> on the earth's surface?",
+    answers:[
+        {text: "Atlantic Ocean", correct: false},
+        {text: "Pacific Ocean", correct: false},
+        {text: "Southern Ocean", correct: false},
+        {text: "Indian Ocean", correct: true},
+    ]
+},
+{
+    question:"Which part of the ocean layer is the flattest and smoothest in the world?",
+    answers:[
+        {text: "Trenches", correct: false},
+        {text: "Deep sea plains", correct: true},
+        {text: "Continental slope", correct: false},
+        {text: "Continental shelf", correct: false},
+    ]
+},
+{
+    question:"With which of the following oceans would you associate the 'Ring of Fire'?",
+    answers:[
+        {text: "Arctic", correct: false},
+        {text: "Indian", correct: false},
+        {text: "Atlantic", correct: false},
+        {text: "Pacific", correct: true},
+    ]
+},
+{
+    question:"Approximately how much percentage (near to integer) of total water on the surface of earth is accounted for the oceans?",
+    answers:[
+        {text: "96", correct: false},
+        {text: "97", correct: true},
+        {text: "94", correct: false},
+        {text: "92", correct: false},
+    ]
+},
+{
+    question:"Madagascar is located in the ______ Ocean.",
+    answers:[
+        {text: "Pacific", correct: false},
+        {text: "Arctic", correct: false},
+        {text: "Indian", correct: true},
+        {text: "Atlantic", correct: false},
+    ]
+},
+{
+    question:"The _______ is the extended margin of each continent occupied by relatively shallow seas and gulfs.",
+    answers:[
+        {text: "deep sea plain", correct: false},
+        {text: "continental slope", correct: false},
+        {text: "oceanic deeps", correct: false},
+        {text: "continental shelf", correct: true},
+    ]
+},
+{
+    question:"Which of the following is a group of islands found in the tropical oceans consisting of coral reefs and a central depression?",
+    answers:[
+        {text: "Guyots", correct: false},
+        {text: "Seamount", correct: false},
+        {text: "Atoll", correct: true},
+        {text: "Lagoon", correct: false},
+    ]
+},
+{
+    question:"Which of the following is the shallowest part of the ocean showing an average gradient of 1&deg; or even less?",
+    answers:[
+        {text: "Oceanic deeps", correct: false},
+        {text: "Continental slope", correct: false},
+        {text: "Continental shelf", correct: true},
+        {text: "Deep sea plain", correct: false},
+    ]
+},
+{
+    question:"The Northeast Canyons and Seamounts Marine National Monument is located in the",
+    answers:[
+        {text: "Arctic Ocean", correct: false},
+        {text: "Arabian Sea", correct: false},
+        {text: "Atlantic Ocean", correct: true},
+        {text: "Red Sea", correct: false},
+    ]
+},
+{
+    question:"The Black Sea is in the _____________ Ocean.",
+    answers:[
+        {text: "Atlantic", correct: true},
+        {text: "Arctic", correct: false},
+        {text: "Pacific", correct: false},
+        {text: "Indian", correct: false},
+    ]
+},
+{
+    question:"Which of the following statements about tides is INCORRECT",
+    answers:[
+        {text: "High tides occur when the moon is in its first and last quarter", correct: true},
+        {text: "Low tides are also called neap tides.", correct: false},
+        {text: "Tides are caused by the gravitational pull exerted by the sun and the moon on Earth's surface.", correct: false},
+        {text: "Spring tides occur on full moon and new moon days.", correct: false},
+    ]
+},
+{
+    question:"Name the only sea in the world that does NOT have a land boundary.",
+    answers:[
+        {text: "Sargasso Sea", correct: true},
+        {text: "Amundsen Sea", correct: false},
+        {text: "Pechora Sea", correct: false},
+        {text: "Iroise Sea", correct: false},
+    ]
+},
+{
+    question:"Almost 98% of earth's crust is made up of eight elements. Which of the following does NOT figure among these elements?",
+    answers:[
+        {text: "Carbon", correct: true},
+        {text: "Oxygen", correct: false},
+        {text: "Calcium", correct: false},
+        {text: "Silicon", correct: false},
+    ]
+},
+{
+    question:"Which of the following is one of the main causes of tides?",
+    answers:[
+        {text: "Rotation of Earth", correct: false},
+        {text: "Solar flares", correct: false},
+        {text: "Gravitational pull of the Sun and the Moon", correct: true},
+        {text: "Closed orbits of planets", correct: false},
+    ]
+},
+{
+    question:"_______ boundaries occur when plates collide and one plate is pushed under the other.",
+    answers:[
+        {text: "Convergent", correct: true},
+        {text: "Transform", correct: false},
+        {text: "Continental", correct: false},
+        {text: "Divergent", correct: false},
+    ]
+},
+{
+    question:"_____ is made of lower density felsic rocks, such as andesite and granite.",
+    answers:[
+        {text: "Outer core", correct: false},
+        {text: "Inner core", correct: false},
+        {text: "Continental crust", correct: true},
+        {text: "Oceanic crust", correct: false},
+    ]
+},
+{
+    question:"Which of the following was NOT a part of Gondwanaland?",
+    answers:[
+        {text: "Australia", correct: false},
+        {text: "India", correct: false},
+        {text: "South America", correct: false},
+        {text: "North America", correct: true},
+    ]
+},
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuize(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;  
+
+    currentQuestion.answers.forEach(answer => {
+       const button = document.createElement("button");
+       button.innerHTML = answer.text;
+       button.classList.add("ans-btn");
+       answerButtons.appendChild(button); 
+       if(answer.correct){
+        button.dataset.correct = answer.correct;
+       }
+       button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+}else{
+    selectedBtn.classList.add("incorrect");
+}
+Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+    }
+    button.disabled = true;
+});
+nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+handleNextButton();
+    }else{
+        startQuize();
+    }
+});
+
+startQuize();
+
